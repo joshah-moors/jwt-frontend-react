4ec122894d738a5e351b56b245971d2efe356a4c
Joshah <joshah.moors@utexas.edu>
2020-06-10 19:29:58 -0500

    Login Route

--- src/App.js
+++ src/App.js
@@ -11,17 +11,28 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-ro
 import Home from './pages';
 
 
-class App extends Component{
+class LoginPage extends Component {
+  render() {
+    return (
+      <h1>This is the Login Page</h1>
+    )
+  }
+}
+
+
+class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LoginPage} />
         </Switch>
       </Router>
     )
   }
 }
+
 
 
 
