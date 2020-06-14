880fd4ebc0f237eac5fbba4ebc8867e819a5867b
Joshah <joshah.moors@utexas.edu>
2020-06-14 08:53:16 -0500

    Broke out banner component and added to login screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,30 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 
-
-class Banner extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedIn: false,
-      user: 'Llenora',
-    }
-  }
-  render() {
-    return (
-      <div className="banner">
-        <h2 className="banner-logo">JoshahLine Web Solutions</h2>
-        <h2 className="banner-text">
-        {!this.state.loggedIn?
-          <Link className="banner-link" to="/login">Login</Link>
-        :
-          <label>{ this.state.user }</label>
-        }
-        </h2>
-      </div>
-    )
-  }
-}
+import Banner from '../components/banner';
 
 
 class Home extends Component {
