f3a02ed756593d2309993cc2d7824b1df862a608
Joshah <joshah.moors@utexas.edu>
2020-06-14 09:52:17 -0500

    Added Redux store, actions, root reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App.js';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { addArticle } from './redux/actions';
+
+window.store = store;
+window.addArticle = addArticle;
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+    , document.getElementById('root')
+);
